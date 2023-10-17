c2b58f540cb387dd6ee5006e6367c991769c275f
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-17 10:58:31 -0300

    coment

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-//criando/declarando a função tocasompom
+//criando/declarando a função tocaSomPom
 function tocaSomPom(){
     document.querySelector("#som_tecla_pom").play();
     }
+//chamando/invocando a função tocaSomPom pelo clique do botão pom
     document.querySelector(".tecla_pom").onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll(".tecla");    
