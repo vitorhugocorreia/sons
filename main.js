1c377cd0a78c9e6f6b0f4038820a3b7f2e453d2a
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-17 10:56:02 -0300

    tirei

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
+//criando/declarando a função tocasompom
 function tocaSomPom(){
     document.querySelector("#som_tecla_pom").play();
     }
     document.querySelector(".tecla_pom").onclick = tocaSomPom;
 
-function tocaSomClap(){
-    document.querySelector("#som_tecla_clap").play();
-    }
-    document.querySelector(".tecla_clap").onclick = tocaSomClap;
-
 const listaDeTeclas = document.querySelectorAll(".tecla");    
 listaDeTeclas[0].onclick = tocaSomPom;
-    
-    let contador = 0;
-    // Estrutura de repetição - Enquanto
-    while() {
-    }
