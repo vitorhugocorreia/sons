2e5a22e37df713a09ce57dd28a01d46af0d2532d
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-17 11:04:28 -0300

    comentários

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ function tocaSomPom(){
 //chamando/invocando a função tocaSomPom pelo clique do botão pom
     document.querySelector(".tecla_pom").onclick = tocaSomPom;
 
+//criando referência constante listaDeTeclas com buscando todos os itens pela classe .tecla 
 const listaDeTeclas = document.querySelectorAll(".tecla");    
+//invocando a função tocaSomPom a partir do item "0" , 1º botão
 listaDeTeclas[0].onclick = tocaSomPom;
