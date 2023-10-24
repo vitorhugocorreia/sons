3554a99b94e115db0d75d63aafa44e096e18ff19
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-24 11:25:40 -0300

    foto

--- main.js
+++ main.js
@@ -15,10 +15,7 @@ let contador = 0;
 // Estrutura de repetição - Enquanto
 while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = tocaSom;
-    listaDeTeclas[contador].onclick = function () {
-
-        tocaSom("#som_tecla_splash");
-        };
+    
     contador = contador + 1;
 
 }
