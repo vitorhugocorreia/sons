8d7555204a70ac49fc5e9741f7cb37dd58edb99f
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-24 11:13:27 -0300

    ag

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ listaDeTeclas[0].onclick = tocaSomPom;
 let contador = 0;
 // Estrutura de repetição - Enquanto
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom;
 
     contador = contador + 1;
 
