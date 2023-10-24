cd55421f3726a16338c80df47a5941a2586da655
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-24 11:28:58 -0300

    muda 5º cment

--- main.js
+++ main.js
@@ -2,12 +2,14 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
     }
-
 //criando referência constante listaDeTeclas com buscando todos os itens pela classe .tecla 
 const listaDeTeclas = document.querySelectorAll(".tecla");    
+//invocando a função tocaSomPom a partir do item "0" , 1º botão
+listaDeTeclas[0].onclick = tocaSom;
 
 //criando referência variável "contador" iniciando = 0
 let contador = 0;
+
 // Estrutura de repetição - Enquanto
 while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = tocaSom;
