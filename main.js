7bac86340c917015529a650c6b751db5cf39a457
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-24 11:27:23 -0300

    toca som pom apagado

--- main.js
+++ main.js
@@ -2,13 +2,9 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
     }
-//chamando/invocando a função tocaSomPom pelo clique do botão pom
-    document.querySelector(".tecla_pom").onclick = tocaSomPom;
 
 //criando referência constante listaDeTeclas com buscando todos os itens pela classe .tecla 
 const listaDeTeclas = document.querySelectorAll(".tecla");    
-//invocando a função tocaSomPom a partir do item "0" , 1º botão
-listaDeTeclas[0].onclick = tocaSomPom;
 
 //criando referência variável "contador" iniciando = 0
 let contador = 0;
