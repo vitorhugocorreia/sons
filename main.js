08f29ee996a1a00a6458472677269b809f5d3a72
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-10-24 10:53:40 -0300

    slide 11

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ listaDeTeclas[0].onclick = tocaSomPom;
 //criando referência variável "contador" iniciando = 0
 let contador = 0;
 // Estrutura de repetição - Enquanto
-while(){
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
 
+    console.log(contador);
 }
