f9eb47b6a241982757d6d8cadee65b4032949ec2
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-11-07 10:57:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ listaDeTeclas[0].onclick = tocaSom;
 let contador = 0;
 
 // Estrutura de repetição - Enquanto
-while(contador < listaDeTeclas.length){
+while(contador < listaDeTeclas.length);{
    const efeito = listaDeTeclas [contador].classList(1);
    const idAudio = "#som_"+ efeito;
-   listaDeTeclas [contador]. onclick= function(){
+   listaDeTeclas[contador].onclick= function(){
           tocaSom(idAudio);
    } 
-   contador contador + 1
+   contador = contador + 1
    console.log(contador);
 }
