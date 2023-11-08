5ef2317f0f30b6d9992b8e8af1087b8c56261138
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-11-08 09:18:21 -0300

    $$$

--- main.js
+++ main.js
@@ -12,11 +12,12 @@ let contador = 0;
 //estrutura de repetição "while" significa 
 //"enquanto algo acontecer"
 while(contador < listaDeTeclas.length ){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_"+ efeito;
-    listaDeTeclas[contador].onclick = function(){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function(){
         tocaSom(idAudio);
     }
     contador = contador + 1;
     console.log(contador);
-}
+}       
