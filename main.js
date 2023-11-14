1306d1ccf36f7769ed725d038f90bdc646901304
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-11-14 11:13:36 -0300

    onkeydown

--- main.js
+++ main.js
@@ -14,5 +14,7 @@ for(let contador = 0;contador < listaDeTeclas.length; contador = contador++){
     tecla.onclick = function(){ //cria função anônima para chamar tocaSom
         tocaSom(idAudio);
     }
-   
+   tecla.onkeydown = function (){
+    tecla.classList.add('ativa');
+   }
 }
