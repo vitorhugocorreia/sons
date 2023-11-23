e57ea824fd8e332e001ead4cff7322baa726e9ca
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2023-11-23 17:43:49 -0300

    💋

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 //Criando/declarando a função tocaSom com parâmetro idElementoAudio
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    //if else elemento operadores comparação
+        const elemento = document.querySelector(idElementoAudio);
+        if(elemento && elemento.localName === 'audio'){
+           elemento.play(); 
+        }else{
+        console.log("Elemento não encontrado");
+    }
 }
+
 //criando referência constante listaDeTeclas buscando 
 //todos os itens pela classe .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 //estrutura de repetição "while" significa 
-//"enquanto algo acontecer" mudou o while para "for"
-for(let contador = 0;contador < listaDeTeclas.length; contador++){
+//"enquanto algo acontecer" mudou while para "for"
+for(let contador = 0;contador < listaDeTeclas.length;contador++){
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1]; //acessa o 2º item da classe de teclas
     const idAudio = `#som_${efeito}`; //template string
     tecla.onclick = function(){ //cria função anônima para chamar tocaSom
         tocaSom(idAudio);
     }
-   tecla.onkeydown = function (){
-    if(EventCounts.code != 'Tab'){
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }  
     }
-   }
-   tecla.onkeyup = function(){
-    tecla.classList.remove('ativa');
-   }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
-Arrumar
